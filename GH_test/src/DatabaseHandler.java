ef61fc88b7a700016917ad759c5d08c7e698079b
liam-alho <81765597+liam-alho@users.noreply.github.com>
2021-04-29 14:11:23 +0300

    Create DatabaseHandler.java

---
+++ GH_test/src/DatabaseHandler.java
@@ -0,0 +1,54 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/** Creating Database login info */
+
+public class DatabaseHandler {
+    private Connection con;
+    private String DBURL = "jdbc:mysql://localhost:3308/statements?autoReconnect=true&useSSL=false";
+    private String DBUser = "root";
+    private String DBPassword = "root";
+    
+    public DatabaseHandler()
+    {
+        try { 
+            connection();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DatabaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    private void connection() throws ClassNotFoundException,SQLException
+    { 
+            Class.forName("com.mysql.jdbc.Driver");
+            con=(Connection) DriverManager.getConnection(DBURL, DBUser, DBPassword);
+    } 
+
+/** Sending login info to Database and checking if its true or false */
+    
+    public boolean login(String username,String password)
+    {
+        try{
+            String query = "select * from login where username ='" + username + "'  and password='" + password + "'";
+            Statement stmt = null;
+            stmt = (Statement) con.createStatement();
+            ResultSet rs=stmt.executeQuery(query);
+            if (rs.next())
+                return true;
+            else
+                return false;
+        }
+        catch(Exception e)
+        {
+            System.err.println(e);
+            return false;
+        }
+    }
+}
